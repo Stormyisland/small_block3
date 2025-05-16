41fea70126a03f783c88c0eb9f6faee16ce60f0a
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-05-16 00:00:18 -0700

    Update sb3.py

--- sb3.py
+++ sb3.py
@@ -20,8 +20,63 @@ class Block:
 class Blockchain:
   difficuty = 2
   reward = 10
+  
   def __int__(self):
     self.unconfermed_transactions = []
     self.chain =[]
     self.create_genesis_block()
-  def 
+    
+  def create_genesis_block(self):
+    gwenesis_block = Block(0, [], time.time(), "0")
+    self.chain.append(genesis_block)
+
+  def last_block(self):
+    return self.chain[-1]
+
+  def add_transaction(self,transaction):
+    self.unconfirmed_transactions.append(transaction)
+
+  def mine(self, miner_address):
+    if not self.unconfirmed_transactions.append(transactions)
+       return False
+    # Add reward for miner 
+reward_tx = {
+  'from' : 'network'
+  'to': miner_address,
+  'amount': self.reward,
+  'signature':''
+}
+self.unconfirmed_tranactions.append(reward_tx)
+
+last_block = self.last_block()
+new_block = Block(insex=last_block.hash)
+
+     while not new_block.hash.startswith('0' * Blockchain.difficulty):
+       new_block.nonce += 1
+       new_block.hash = new_block.compute_hash()
+
+      self.chain.append(new_block)
+      sefl.uncomfirme_transactions = []
+      return new_block 
+
+class wallet:
+  def __init__(self):
+    (self.public_key, self.privat_key) = rsa.newkeys(512)
+   
+  
+  
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+  
